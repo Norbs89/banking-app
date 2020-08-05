fe1e087a2f28f32a5ed41a745e015e91fefd40a3
Norbs89 <n_huvos@live.co.uk>
2020-08-05 18:36:05 +0100

    Added custom amount field

--- src/components/BalanceHandler.jsx
+++ src/components/BalanceHandler.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import QuickAddButton from "./QuckAddButton";
+import AddAmountForm from "./AddAmountForm";
 
 class BalanceHandler extends React.Component {
   state = {
     currentBalance: "0.00",
     currency: "£",
     quickAddOpt: ["10", "25", "50", "100"],
+    addValue: "",
+    withdrawValue: "",
+    warning: false,
   };
 
   addBalance = (number) => {
@@ -19,25 +23,61 @@ class BalanceHandler extends React.Component {
     });
   };
 
+  handleInput = (value, name) => {
+    this.setState((currentState) => {
+      return {
+        ...currentState,
+        [name]: value,
+        warning: false,
+      };
+    });
+  };
+
+  addAmount = (e) => {
+    e.preventDefault();
+    if (this.state.addValue.match(/^\d+(\.\d+)?$/)) {
+      this.addBalance(Number(this.state.addValue));
+      this.setState({ addValue: "" });
+    } else {
+      this.setState({ warning: true });
+    }
+  };
+
   render() {
-    const { currency, currentBalance, quickAddOpt } = this.state;
+    const {
+      currency,
+      currentBalance,
+      quickAddOpt,
+      addValue,
+      warning,
+    } = this.state;
     return (
       <div>
-        <section>
+        <section className="balanceDisplay">
           Current Balance: {currency}
           {currentBalance}
         </section>
-        <section>
+        <section className="quickAdd">
           Quick Add:
           {quickAddOpt.map((number) => {
             return (
               <QuickAddButton
+                key={number}
                 currency={currency}
                 addBalance={this.addBalance}
                 amount={number}
               />
             );
           })}
+        </section>
+        <section>
+          <AddAmountForm
+            addAmount={this.addAmount}
+            handleInput={this.handleInput}
+            value={addValue}
+            warning={warning}
+            currency={currency}
+          />
         </section>
       </div>
     );
